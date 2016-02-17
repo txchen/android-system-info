b24bdc5dff36f462e21207d5a4eb4c480712e1e9
Tianxiang Chen <txchendev@gmail.com>
2016-02-16 16:42:22 -0800

    get android_id

--- app/src/main/java/com/tc/systeminfo/MainActivity.java
+++ app/src/main/java/com/tc/systeminfo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.tc.systeminfo;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView)findViewById(R.id.textViewInfo);
+        tv.setText("SystemInfo:\n");
+
+        // android device id
+        tv.append("\nANDROID_ID = " + Settings.Secure.getString(this.getBaseContext().getContentResolver(),
+                Settings.Secure.ANDROID_ID));
+        
+        // GSF id
+
+        // IMEI
+
+        // Sim Subscriber ID
+
+        // Sim Card Serial
+
+        // Local IP addresses
+
+        // Wi-Fi MAC
+
+        // Bluetooth MAC
+
+        // Hardware Serial
+
+        // Device Build Fingerprints
+
+        // Manufacture
+        // Model
+        // Device
+
+        // User Agent * 3
+
+        // CPU_ABI
+
+        // Version Codename
+        // Version Incremental
+        // Version Sdk
+
+        // Display Density
+        // Display Density Dpi
+        // Display Density xDpi
+        // Display Density yDpi
+        // Display widthPixels
+        // Display heightPixels
     }
 }
